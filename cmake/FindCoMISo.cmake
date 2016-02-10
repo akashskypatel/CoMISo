ef85d6954e27ab96a5255740fcd8b593724aa691
Jan Möbius <moebius@cs.rwth-aachen.de>
2016-02-10 10:20:12 +0100

    Removed debug message

--- cmake/FindCoMISo.cmake
+++ cmake/FindCoMISo.cmake
@@ -35,8 +35,6 @@ FIND_PATH( COMISO_CONFIG_INCLUDE_DIR CoMISo/Config/config.hh
                  "${CMAKE_BINARY_DIR}/../CoMISo/" 
                  "${COMISO_INCLUDE_DIR}" )
 
-message("COMISO_CONFIG_INCLUDE_DIR: ${COMISO_CONFIG_INCLUDE_DIR}")
-
 if ( COMISO_INCLUDE_DIR AND COMISO_CONFIG_INCLUDE_DIR )
 
   # add COMISO_INCLUDE_DIR/CoMISo so stuff in CoMISo/Base can be included by <Base/...>
