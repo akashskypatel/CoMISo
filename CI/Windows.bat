25663282a73fe4829917c7d904530a9b608fba92
Jan Möbius <moebius@cs.rwth-aachen.de>
2016-10-20 08:51:06 +0200

    Initial Windows CI

---
+++ CI/Windows.bat
@@ -0,0 +1,60 @@
+
+mkdir rel
+cd rel
+
+IF "%ARCHITECTURE%" == "x64" (
+  set ARCH_VS= Win64
+  set STRING_ARCH=64-Bit
+) else (
+  set ARCH_VS=
+  set STRING_ARCH=32-Bit
+)
+
+
+IF "%QT_VERSION%" == "Qt5.3.1" (
+ set QT_REV=5.3
+ set QT_SUFFIX=_opengl
+)
+
+IF "%QT_VERSION%" == "Qt5.5.1" (
+ set QT_REV=5.5
+ set QT_SUFFIX=
+)
+
+
+IF "%BUILD_PLATFORM%" == "VS2013" (
+    set LIBPATH=C:/libs/VS2013
+    set GTESTVERSION=gtest-1.6.0
+    set GENERATOR=Visual Studio 12%ARCH_VS%
+    set VS_PATH="C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.com"
+    IF "%ARCHITECTURE%" == "x64" (
+      set QT_BASE_CONFIG=-DQT5_INSTALL_PATH=C:\Qt\%QT_VERSION%-vs2013-%STRING_ARCH%\%QT_REV%\msvc2013_64%QT_SUFFIX% 
+    )
+
+    IF "%ARCHITECTURE%" == "x32" (
+      set QT_BASE_CONFIG=-DQT5_INSTALL_PATH=C:\Qt\%QT_VERSION%-vs2013-%STRING_ARCH%\%QT_REV%\msvc2013%QT_SUFFIX%
+    )
+
+    SET BOOST_ROOT=
+) 
+
+IF "%BUILD_PLATFORM%" == "VS2015" (
+    set LIBPATH=C:/libs/VS2015
+    set GTESTVERSION=gtest-1.7.0
+    set GENERATOR=Visual Studio 14%ARCH_VS%
+    set VS_PATH="C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.com"
+    set QT_BASE_CONFIG=TODO
+) 
+
+set CMAKE_CONFIGURATION=%QT_BASE_CONFIG% -DGLUT_INCLUDE_DIR="%LIBPATH%\%ARCHITECTURE%\freeglut-2.8.1\include" -DGLUT_glut_LIBRARY="%LIBPATH%\%ARCHITECTURE%\freeglut-2.8.1\lib\freeglut.lib" -DGLEW_INCLUDE_DIR="%LIBPATH%\%ARCHITECTURE%\glew-1.10.0\include" -DGLEW_LIBRARY="%LIBPATH%\%ARCHITECTURE%\glew-1.10.0\lib\glew32.lib" -DBOOST_ROOT="%LIBPATH%/%ARCHITECTURE%/boost_1_59_0" -DBOOST_LIBRARYDIR="%LIBPATH%/%ARCHITECTURE%/boost_1_59_0/lib64-msvc-12.0" -DOPENFLIPPER_ENABLE_LICENSE_MANAGER=FALSE -DCGAL_INCLUDE_DIR="%LIBPATH%/%ARCHITECTURE%/CGAL-4.7/include" -DCGAL_LIBRARY_DIR="%LIBPATH%/%ARCHITECTURE%/CGAL-4.7/lib" -DCGAL_BIN_DIR="%LIBPATH%/%ARCHITECTURE%/CGAL-4.7/bin" -DOPENFLIPPER_ENABLE_LICENSE_KEY_GENERATOR=FALSE -DOPENFLIPPER_ENABLE_LICENSE_MANAGER=FALSE -DPOCO_DIR_SEARCH="%LIBPATH%/%ARCHITECTURE%/poco-1.6.1-all" -DPoco_LIBRARY_DIR="%LIBPATH%/%ARCHITECTURE%/poco-1.6.1-all/lib" -DPoco_LIBRARY_DIRs="%LIBPATH%/%ARCHITECTURE%/poco-1.6.1-all/bin"  -DPoco_INCLUDE_DIR="%LIBPATH%/%ARCHITECTURE%/poco-1.6.1-all/include"
+
+
+"C:\Program Files (x86)\CMake\bin\cmake.exe"  -DGTEST_PREFIX="%LIBPATH%\%ARCHITECTURE%\%GTESTVERSION%" -G "%GENERATOR%"  -DCMAKE_BUILD_TYPE=Release -DOPENFLIPPER_BUILD_UNIT_TESTS=TRUE %CMAKE_CONFIGURATION% ..
+
+IF %errorlevel% NEQ 0 exit /b %errorlevel%
+
+
+%VS_PATH% /Build "Release" CoMISo.sln /Project "ALL_BUILD"
+
+IF %errorlevel% NEQ 0 exit /b %errorlevel%
+
