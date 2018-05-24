e9c4cb9618a052584820845e087b4cb7e8cac614
Jan Möbius <moebius@cs.rwth-aachen.de>
2018-05-24 13:04:59 +0200

    Updated Mumps finder

--- cmake/FindMUMPS.cmake
+++ cmake/FindMUMPS.cmake
@@ -37,6 +37,7 @@ endif()
 find_path(MUMPS_INCLUDE_DIR NAMES dmumps_c.h
      PATHS "$ENV{IPOPT_HOME}/ThirdParty/Mumps/MUMPS/include/"
            "/usr/include/"
+		   "${VS_SEARCH_PATH}Ipopt-3.12.9/include/mumps"
            "${VS_SEARCH_PATH}Ipopt-3.12.4/Ipopt/MSVisualStudio/v8-ifort/installed/include/mumps/include/"
    )
    
@@ -44,6 +45,7 @@ find_library( MUMPS_LIBRARY_DEBUG
               dmumpsd coinmumpsd coinmumpscd
               PATHS "$ENV{IPOPT_HOME}/lib/"
                     "/usr/lib"
+					"${VS_SEARCH_PATH}Ipopt-3.12.9/lib/"
                     "${VS_SEARCH_PATH}Ipopt-3.12.4/Ipopt/MSVisualStudio/v8-ifort/installed/lib"
                     )
                     
@@ -51,6 +53,7 @@ find_library( MUMPS_LIBRARY_RELEASE
               dmumps coinmumps coinmumpsc
               PATHS "$ENV{IPOPT_HOME}/lib/"
                     "/usr/lib"
+					"${VS_SEARCH_PATH}Ipopt-3.12.9/lib/"
                     "${VS_SEARCH_PATH}Ipopt-3.12.4/Ipopt/MSVisualStudio/v8-ifort/installed/lib"
                     )                    
 
