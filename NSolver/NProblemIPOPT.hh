cea5978d572431872448a42b2bf0ef422417c976
David Bommes <bommes@cs.rwth-aachen.de>
2018-11-14 18:01:27 +0100

    fixed compiler warnings related to missing override tags

--- NSolver/NProblemIPOPT.hh
+++ NSolver/NProblemIPOPT.hh
@@ -76,26 +76,26 @@ public:
   //@{
   /** Method to return some info about the nlp */
   virtual bool get_nlp_info(Index& n, Index& m, Index& nnz_jac_g,
-                            Index& nnz_h_lag, IndexStyleEnum& index_style);
+                            Index& nnz_h_lag, IndexStyleEnum& index_style) override;
 
   /** Method to return the bounds for my problem */
   virtual bool get_bounds_info(Index n, Number* x_l, Number* x_u,
-                               Index m, Number* g_l, Number* g_u);
+                               Index m, Number* g_l, Number* g_u) override;
 
   /** Method to return the starting point for the algorithm */
   virtual bool get_starting_point(Index n, bool init_x, Number* x,
                                   bool init_z, Number* z_L, Number* z_U,
                                   Index m, bool init_lambda,
-                                  Number* lambda);
+                                  Number* lambda) override;
 
   /** Method to return the objective value */
-  virtual bool eval_f(Index n, const Number* x, bool new_x, Number& obj_value);
+  virtual bool eval_f(Index n, const Number* x, bool new_x, Number& obj_value) override;
 
   /** Method to return the gradient of the objective */
-  virtual bool eval_grad_f(Index n, const Number* x, bool new_x, Number* grad_f);
+  virtual bool eval_grad_f(Index n, const Number* x, bool new_x, Number* grad_f) override;
 
   /** Method to return the constraint residuals */
-  virtual bool eval_g(Index n, const Number* x, bool new_x, Index m, Number* g);
+  virtual bool eval_g(Index n, const Number* x, bool new_x, Index m, Number* g) override;
 
   /** Method to return:
    *   1) The structure of the jacobian (if "values" is NULL)
@@ -103,7 +103,7 @@ public:
    */
   virtual bool eval_jac_g(Index n, const Number* x, bool new_x,
                           Index m, Index nele_jac, Index* iRow, Index *jCol,
-                          Number* values);
+                          Number* values) override;
 
   /** Method to return:
    *   1) The structure of the hessian of the lagrangian (if "values" is NULL)
@@ -112,7 +112,7 @@ public:
   virtual bool eval_h(Index n, const Number* x, bool new_x,
                       Number obj_factor, Index m, const Number* lambda,
                       bool new_lambda, Index nele_hess, Index* iRow,
-                      Index* jCol, Number* values);
+                      Index* jCol, Number* values) override;
 
   //@}
 
@@ -124,7 +124,7 @@ public:
                                  Index m, const Number* g, const Number* lambda,
                                  Number obj_value,
                                  const IpoptData* ip_data,
-                                 IpoptCalculatedQuantities* ip_cq);
+                                 IpoptCalculatedQuantities* ip_cq) override;
   //@}
 
   /** Intermediate Callback method for the user.  Providing dummy
@@ -234,26 +234,26 @@ public:
   //@{
   /** Method to return some info about the nlp */
   virtual bool get_nlp_info(Index& n, Index& m, Index& nnz_jac_g,
-                            Index& nnz_h_lag, IndexStyleEnum& index_style);
+                            Index& nnz_h_lag, IndexStyleEnum& index_style) override;
 
   /** Method to return the bounds for my problem */
   virtual bool get_bounds_info(Index n, Number* x_l, Number* x_u,
-                               Index m, Number* g_l, Number* g_u);
+                               Index m, Number* g_l, Number* g_u) override;
 
   /** Method to return the starting point for the algorithm */
   virtual bool get_starting_point(Index n, bool init_x, Number* x,
                                   bool init_z, Number* z_L, Number* z_U,
                                   Index m, bool init_lambda,
-                                  Number* lambda);
+                                  Number* lambda) override;
 
   /** Method to return the objective value */
-  virtual bool eval_f(Index n, const Number* x, bool new_x, Number& obj_value);
+  virtual bool eval_f(Index n, const Number* x, bool new_x, Number& obj_value) override;
 
   /** Method to return the gradient of the objective */
-  virtual bool eval_grad_f(Index n, const Number* x, bool new_x, Number* grad_f);
+  virtual bool eval_grad_f(Index n, const Number* x, bool new_x, Number* grad_f) override;
 
   /** Method to return the constraint residuals */
-  virtual bool eval_g(Index n, const Number* x, bool new_x, Index m, Number* g);
+  virtual bool eval_g(Index n, const Number* x, bool new_x, Index m, Number* g) override;
 
   /** Method to return:
    *   1) The structure of the jacobian (if "values" is NULL)
@@ -261,7 +261,7 @@ public:
    */
   virtual bool eval_jac_g(Index n, const Number* x, bool new_x,
                           Index m, Index nele_jac, Index* iRow, Index *jCol,
-                          Number* values);
+                          Number* values) override;
 
   /** Method to return:
    *   1) The structure of the hessian of the lagrangian (if "values" is NULL)
@@ -270,7 +270,7 @@ public:
   virtual bool eval_h(Index n, const Number* x, bool new_x,
                       Number obj_factor, Index m, const Number* lambda,
                       bool new_lambda, Index nele_hess, Index* iRow,
-                      Index* jCol, Number* values);
+                      Index* jCol, Number* values) override;
 
   //@}
 
@@ -282,7 +282,7 @@ public:
                                  Index m, const Number* g, const Number* lambda,
                                  Number obj_value,
                                  const IpoptData* ip_data,
-                                 IpoptCalculatedQuantities* ip_cq);
+                                 IpoptCalculatedQuantities* ip_cq) override;
   //@}
 
   /** Intermediate Callback method for the user.  Providing dummy
