17bb847169b68ba2184d183031d08c9461ad2fa1
Martin Heistermann <git@mheistermann.de>
2019-09-03 15:56:51 +0200

    CMake improvements;
    - always set project name
    - be more careful setting include directories
    - install targets and version file, export package
    - CMakeConfig file: only include targets.cmake if target does not exist yet.

--- cmake/CoMISoConfig.cmake
+++ cmake/CoMISoConfig.cmake
@@ -1,4 +1,4 @@
-include(CMakeFindDependencyMacro)
+if(NOT TARGET CoMISo::CoMISo)
+    include("${CMAKE_CURENT_LIST_DIR}/CoMISoTargets.cmake")
+endif()
 
-# Add the targets file
-include("${CMAKE_CURRENT_LIST_DIR}/CoMISoTargets.cmake")
