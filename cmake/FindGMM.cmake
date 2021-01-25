5fe64454b1378240b47685e193d2fb7fdf93d409
Max Lyon <lyon@cs.rwth-aachen.de>
2021-01-25 19:55:56 +0100

    update gmm search paths

--- cmake/FindGMM.cmake
+++ cmake/FindGMM.cmake
@@ -32,6 +32,8 @@ find_path( GMM_INCLUDE_DIR
                  "c:\\libs\\gmm-3.0\\include"
                  "${CMAKE_WINDOWS_LIBS_DIR}/general/gmm-5.0/include"
                  "${CMAKE_WINDOWS_LIBS_DIR}/general/gmm-4.2/include"
+                 "${VCI_WINDOWS_LIBS_DIR}/general/gmm-5.0/include"
+                 "${VCI_WINDOWS_LIBS_DIR}/general/gmm-4.2/include"
 		 ${PROJECT_SOURCE_DIR}/MacOS/Libs/gmm-3.1/include
                  ../../External/include
                  /ACG/acgdev/gcc-4.3-i686/gmm-4.1/include/
