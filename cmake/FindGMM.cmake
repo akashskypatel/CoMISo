3089de965262c6fb5e59b3b642ab2cf71436c2d6
Max Lyon <lyon@cs.rwth-aachen.de>
2021-02-15 20:32:18 +0100

    update gmm finder for version 5.4

--- cmake/FindGMM.cmake
+++ cmake/FindGMM.cmake
@@ -34,6 +34,7 @@ find_path( GMM_INCLUDE_DIR
                  "${CMAKE_WINDOWS_LIBS_DIR}/general/gmm-5.4/include"
                  "${CMAKE_WINDOWS_LIBS_DIR}/general/gmm-5.0/include"
                  "${CMAKE_WINDOWS_LIBS_DIR}/general/gmm-4.2/include"
+                 "${VCI_WINDOWS_LIBS_DIR}/general/gmm-5.4/include"
                  "${VCI_WINDOWS_LIBS_DIR}/general/gmm-5.0/include"
                  "${VCI_WINDOWS_LIBS_DIR}/general/gmm-4.2/include"
 		 ${PROJECT_SOURCE_DIR}/MacOS/Libs/gmm-3.1/include
